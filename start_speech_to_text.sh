b6441ac935fb80c1bba105f7e14a0010fc84f013
eliaspfeffer <eliaspfeffer@gmail.com>
2025-05-25 13:13:26 +0200

    Remove cursorfinishsound subproject and comment out LD_LIBRARY_PATH export in start_speech_to_text.sh

--- start_speech_to_text.sh
+++ start_speech_to_text.sh
@@ -10,7 +10,7 @@ cd "$PROJECT_DIR"
 source venv/bin/activate
 
 # Exportieren der LD_LIBRARY_PATH-Variable für den Fall, dass später GPU-Unterstützung benötigt wird
-export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib 2>/dev/null; import nvidia.cudnn.lib 2>/dev/null; paths = []; p1 = os.path.dirname(nvidia.cublas.lib.__file__) if "nvidia.cublas.lib" in sys.modules else ""; p2 = os.path.dirname(nvidia.cudnn.lib.__file__) if "nvidia.cudnn.lib" in sys.modules else ""; if p1: paths.append(p1); if p2: paths.append(p2); print(":".join(paths))' 2>/dev/null` || true
+#export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib 2>/dev/null; import nvidia.cudnn.lib 2>/dev/null; paths = []; p1 = os.path.dirname(nvidia.cublas.lib.__file__) if "nvidia.cublas.lib" in sys.modules else ""; p2 = os.path.dirname(nvidia.cudnn.lib.__file__) if "nvidia.cudnn.lib" in sys.modules else ""; if p1: paths.append(p1); if p2: paths.append(p2); print(":".join(paths))' 2>/dev/null` || true
 
 # Starten des Programms
 python3 realtime_speech_to_text.py --model small --device cpu --compute_type int8
